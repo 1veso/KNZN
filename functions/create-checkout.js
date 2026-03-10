61d764a9e9dcd7cf24ad2c41a995d006502b669f
1veso <pvesenja@smail.uni-koeln.de>
2026-03-10 15:56:41 +0100

    feat: wire Stripe price IDs, metadata, Sonderangebot coupon logic

--- functions/create-checkout.js
+++ functions/create-checkout.js
@@ -3,7 +3,7 @@ export async function onRequestPost(context) {
 
   try {
     const body = await request.json();
-    const { plateText, plateFormat, material, addons } = body;
+    const { plateText, plateFormat, material, size, type, addons } = body;
 
     const origin = new URL(request.url).origin;
     const params = new URLSearchParams();
@@ -14,35 +14,44 @@ export async function onRequestPost(context) {
 
     let i = 0;
 
-    // Kennzeichen
-    params.set(`line_items[${i}][price_data][currency]`, 'eur');
-    params.set(`line_items[${i}][price_data][product_data][name]`, `2 Kennzeichen (${material === 'carbon' ? 'Carbon' : 'Standard'}) - ${plateText}`);
-    params.set(`line_items[${i}][price_data][unit_amount]`, material === 'carbon' ? '2000' : '1000');
+    const kennzeichenPriceId = material === 'carbon'
+        ? 'price_1T9RX4JlVSturRwqiugKPXuT'
+        : 'price_1T9RWMJlVSturRwqU5Q3bqDu';
+
+    const sizeLabel = size === 'klein' ? 'Klein' : 'Standard';
+    const typeLabel = type === 'elektro' ? 'Elektro'
+        : type === 'oldtimer' ? 'Oldtimer'
+            : type === 'saisonal' ? 'Saisonal'
+                : 'Standard';
+
+    params.set(`line_items[${i}][price]`, kennzeichenPriceId);
     params.set(`line_items[${i}][quantity]`, '1');
+    params.set(`metadata[kennzeichen_text]`, plateText || '');
+    params.set(`metadata[groesse]`, sizeLabel);
+    params.set(`metadata[typ]`, typeLabel);
     i++;
 
     if (addons.zulassung) {
-      params.set(`line_items[${i}][price_data][currency]`, 'eur');
-      params.set(`line_items[${i}][price_data][product_data][name]`, 'KFZ Zulassung');
-      params.set(`line_items[${i}][price_data][unit_amount]`, '2000');
+      params.set(`line_items[${i}][price]`, 'price_1T9RXdJlVSturRwqIcTAFpni');
       params.set(`line_items[${i}][quantity]`, '1');
       i++;
     }
 
     if (addons.plakette) {
-      params.set(`line_items[${i}][price_data][currency]`, 'eur');
-      params.set(`line_items[${i}][price_data][product_data][name]`, 'Umweltplakette');
-      params.set(`line_items[${i}][price_data][unit_amount]`, '500');
+      params.set(`line_items[${i}][price]`, 'price_1T9RYBJlVSturRwqteAza4Tg');
       params.set(`line_items[${i}][quantity]`, '1');
       i++;
     }
 
     if (addons.versand) {
-      params.set(`line_items[${i}][price_data][currency]`, 'eur');
-      params.set(`line_items[${i}][price_data][product_data][name]`, 'DHL Versand');
-      params.set(`line_items[${i}][price_data][unit_amount]`, '500');
+      params.set(`line_items[${i}][price]`, 'price_1T9RYeJlVSturRwqt1Es6dV4');
       params.set(`line_items[${i}][quantity]`, '1');
       i++;
+    }
+
+    const isSonderangebot = material !== 'carbon' && addons.zulassung && addons.plakette;
+    if (isSonderangebot) {
+      params.set('discounts[0][coupon]', 'Zu15xPHh');
     }
 
     const stripeRes = await fetch('https://api.stripe.com/v1/checkout/sessions', {
